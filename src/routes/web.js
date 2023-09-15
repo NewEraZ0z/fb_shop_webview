c10b2b0cad5b6136a56a357a010ed96b0256cc32
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2023-09-15 20:30:07 +0100

    Update web.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -8,6 +8,7 @@ let initWebRoutes = (app) => {
     router.get("/", homepageController.getHomepage);
     router.get("/webhook", homepageController.getWebhook);
     router.post("/webhook", homepageController.postWebhook);
+    router.get("/webview", homepageController.getWebViewPage);
 
     return app.use("/", router);
 };
