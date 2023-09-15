a00bbf2582c329fdc951b6ef9d8b8da6e65ea8ab
FantasyZoom22 <fantasyzoom22@gmail.com>
2023-09-15 17:35:59 +0100

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,27 +1,19 @@
 require('dotenv').config();
-<<<<<<< HEAD
-import express from "express";
-import configViewEngine from "./config/viewEngine";
-import initWebRoutes from "./routes/web";
-import bodyParser from "body-parser";
-=======
-
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const initWebRoutes = require("./routes/web");
 const bodyParser = require("body-parser");
->>>>>>> 2221c31f22d3e7512d0f37502adc7f6667ddc30b
 
 let app = express();
 
-//config body-parser to post data
+// config body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//config view engine
+// config view engine
 configViewEngine(app);
 
-//config web routes
+// config web routes
 initWebRoutes(app);
 
 let port = process.env.PORT || 8000;
