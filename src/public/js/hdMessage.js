c37b1c4bf511fab8a73cada4ceabd326d191a765
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2023-09-20 23:23:45 +0100

    Update hdMessage.js

--- src/public/js/hdMessage.js
+++ src/public/js/hdMessage.js
@@ -1,4 +1,3 @@
-
 (function(d, s, id){
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
@@ -8,36 +7,28 @@
 }(document, 'script', 'Messenger'));
 
 
+
 window.extAsyncInit = function() {
- MessengerExtensions.getSupportedFeatures(function success(result) {
-  let features = result.supported_features;
-  if (features.indexOf("context") != -1) {
-    MessengerExtensions.getContext('269582959293477',
-      function success(thread_context) {
-        // success
-        let userId = thread_context.psid;
-        document.getElementById("psid").value = userId;
-        // More code to follow
-      },
-      function error(err) {
-        console.log(err);
-      }
-    );
-  }
-}, function error(err) {
-  console.log(err);
-});
+  // the Messenger Extensions JS SDK is done loading 
+  MessengerExtensions.getContext('your app', 
+    function success(thread_context){
+      // success
+      document.getElementById('psid').value =  thread_context.psid; // Store PSID as a global variable
+    },
+    function error(err){
+      // error
+      console.log(err);
+    }
+  );
 };
 
-
-
-  
-function handleSaveBtn() {
-const serverEndpoint = '/setup-webview';
+function handleSaveBtn() { // Pass psid as a parameter
+  // Assuming you have a server endpoint to send the 'foods' data to.
+  const serverEndpoint = '/setup-webview';
 
   // Prepare the data to send to the server
   const requestData = {
-    psid: psid, // Use the retrieved PSID
+    psid: psid, // Use the PSID passed as a parameter
     foods: foods, // Assuming 'foods' is an object with item data as you've defined
   };
 
@@ -49,21 +40,24 @@ const serverEndpoint = '/setup-webview';
     },
     body: JSON.stringify(requestData),
   })
-    
-// Close the webview after making the request
-MessengerExtensions.requestCloseBrowser(function success() {
-}, function error(err) {
-  console.log(err);
-  // an error occurred
-});
-  //jumping-line
     .then((response) => response.json())
     .then((data) => {
       // Handle the response from the server as needed
       console.log(data);
+    })
     .catch((error) => {
       // Handle errors
       console.error(error);
     });
 
+  // Close the webview when the request is initiated
+  MessengerExtensions.requestCloseBrowser(
+    function success() {
+      // Webview closed
+    },
+    function error(err) {
+      // An error occurred while closing the webview
+      console.log(err);
+    }
+  );
 }
