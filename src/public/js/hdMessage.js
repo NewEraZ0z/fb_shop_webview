283a78ea840e1a5b760da57493e6dbd581a639d9
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2023-09-21 15:02:48 +0100

    Update hdMessage.js

--- src/public/js/hdMessage.js
+++ src/public/js/hdMessage.js
@@ -6,60 +6,46 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'Messenger'));
 
-
-
 window.extAsyncInit = function() {
   // the Messenger Extensions JS SDK is done loading 
+  //get user PSID
   MessengerExtensions.getContext('269582959293477', 
-    function success(thread_context){
-      // success
-      document.getElementById('psid').value =  thread_context.psid; // Store PSID as a global variable
-      let userID = thread_context.psid;
-      console.log("PSID:" + userID);
-    },
-    function error(err){
-      // error
-      console.log(err);
-    }
-  );
-};
-
-function handleSaveBtn() { // Pass psid as a parameter
-  // Assuming you have a server endpoint to send the 'foods' data to.
-  const serverEndpoint = '/setup-webview';
-
-  // Prepare the data to send to the server
-  const requestData = {
+  function success(thread_context){
+    let userPSID = thread_context.psid;
+    document.getElementById("psid").value = userPSID;
+    
+  },
+  function error(err){
+    // error
+  }
+);
+  // webview closed
+  document.getElementById("submitBtn").addEventListener('click',function ()
+    {
+     MessengerExtensions.requestCloseBrowser(function success() {
+  
+    // send request
+    let dataBody = {
     psid: document.getElementById("psid").value, // Use the PSID passed as a parameter
     foods: foods, // Assuming 'foods' is an object with item data as you've defined
-  };
-
-  // Make an HTTP POST request to the server
-  fetch(serverEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(requestData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle the response from the server as needed
-      console.log(data);
-    })
-    .catch((error) => {
-      // Handle errors
-      console.error(error);
-    });
-
-  // Close the webview when the request is initiated
-  MessengerExtensions.requestCloseBrowser(
-    function success() {
-      // Webview closed
-    },
-    function error(err) {
-      // An error occurred while closing the webview
-      console.log(err);
     }
-  );
-}
+
+       // request via ajax
+       $.ajax({
+         method: 'POST',
+         data: dataBody,
+         url: '${window.location.origin}/setup-webview',
+         success: function(data){
+           console.log('success response from server ajax:', data)
+         },
+         error: function(error){
+           console.log('error response from server ajax:', error)
+         }
+       })
+       
+}, function error(err) {
+  // an error occurred
+});
+    }  ); 
+  
+};
