f1f85dc67e5cc7988a8bb15a45f6d1c1944f90b6
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2023-09-21 17:35:05 +0100

    Update hdMessage.js

--- src/public/js/hdMessage.js
+++ src/public/js/hdMessage.js
@@ -1,20 +1,20 @@
-  (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.com/en_US/messenger.Extensions.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'Messenger'));
-    window.extAsyncInit = function () {
-        var isSupported = MessengerExtensions.isInExtension(); 
-        alert(isSupported);
-        // the Messenger Extensions JS SDK is done loading
-        MessengerExtensions.getContext('269582959293477', 
-          function success(result){
-            alert("Success: "+result.psid);
-          },
-          function error(result){
-            alert(JSON.stringify(result));
-          }
-        );
-    };
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
+window.extAsyncInit = function() {
+  // the Messenger Extensions JS SDK is done loading 
+  MessengerExtensions.getContext('YOUR_APP_ID', 
+  function success(thread_context){
+    console.log(thread_context.psid);
+    // success
+  },
+  function error(err){
+    // error
+  }
+);
+};
