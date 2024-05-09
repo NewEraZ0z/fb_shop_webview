710fd37b8d49bde6bcd1fcc8ac29184be949a5bb
NewEraZ0z <irchadnextgen.sba@gmail.com>
2024-05-09 12:39:45 +0100

    Update homepageController.js

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -148,7 +148,7 @@ let handleMessage = (sender_psid, received_message) => {
 
 
 // Handles messaging_postbacks events
-let handlePostback = (sender_psid, received_postback) => {
+let handlePostback = async (sender_psid, received_postback) => {
     let response;
 
     // Get the payload for the postback
